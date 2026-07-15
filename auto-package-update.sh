8a06990c85cc461440252a28a1a945e3dd892878
Riva Dias <rivadias93@gmail.com>
2026-07-15 23:40:34 +0530

    auto package updating scrript

---
+++ auto-package-update.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# checking for the root privileges
+if [ "$EUID" -ne 0 ]; then 
+    echo "The script should be run as root"
+    exit 1
+fi
+
+echo "Starting with system update and cleaning process"
+
+apt-get update && apt-get upgrade -y && apt-get autoremove -y && apt-get clean
+
+echo "System packages updated and cleaned up" 
